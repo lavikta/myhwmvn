478496ee3330d686dc7596b834031f47e084066e
Victoria Karetnikova <karetnikova.test@gmail.com>
2023-08-21 12:59:01 -0400

    Second test

--- src/test/java/ru/netology/services/FreelanceServiceTest.java
+++ src/test/java/ru/netology/services/FreelanceServiceTest.java
@@ -11,8 +11,8 @@ class FreelanceServiceTest {
     public void testFreelance1() {
         FreelanceService service = new FreelanceService();
 
-        int actual = service.calculate(10_000, 3_000, 20_000);
-        int expected = 3;
+        int actual = service.calculate(100_000, 60_000, 150_000);
+        int expected = 2;
 
         Assertions.assertEquals(expected, actual);
 
